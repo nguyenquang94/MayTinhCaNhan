1c7e596cd95aaec779b89f9fc1c8c05f318acb27
Quang Nguyễn Ngọc <quangnv@Quangs-MacBook-Pro.local>
2017-01-01 07:18:50 +0700

    frist

---
+++ App.js
@@ -0,0 +1,160 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import ReactCalculator from './src/ReactCalculator';
+import Style from './src/Style';
+import InputButton from './src/InputButton';
+import { AdMobBanner } from 'react-native-admob';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const inputButtons = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+'],
+    ['c', 'ce']
+];
+export default class App extends Component<{}> {
+  constructor(props) {
+        super(props);
+
+        this.initialState = {
+            previousInputValue: 0,
+            inputValue: 0,
+            selectedSymbol: null
+        };
+
+        this.state = this.initialState;
+    }
+
+    render() {
+        return (
+            <View style={Style.rootContainer}>
+                <View style={Style.displayContainer}>
+                    <Text style={Style.displayText}>{this.state.inputValue}</Text>
+                </View>
+                <View style={Style.inputContainer}>
+                    {this._renderInputButtons()}
+                </View>
+                 <View style={{flex: 1, alignItems: 'center',backgroundColor: 'white', marginLeft: 20, marginRight: 20 }}>
+                        <AdMobBanner
+                            adSize="banner"
+                            adUnitID="ca-app-pub-8721382518026083/4263463398"
+                            testDeviceID="EMULATOR"
+                            didFailToReceiveAdWithError={this.bannerError}/>
+                    </View>
+            </View>
+        );
+    }
+
+    _renderInputButtons() {
+
+        let views = inputButtons.map((row, idx) => {
+            let inputRow = row.map((buttonVal, columnIdx) => {
+                return <InputButton
+                            value={buttonVal}
+                            highlight={this.state.selectedSymbol === buttonVal}
+                            onPress={this._onInputButtonPressed.bind(this, buttonVal)}
+                            key={'butt-' + columnIdx} />;
+            });
+
+            return <View style={Style.inputRow} key={'row-' + idx}>{inputRow}</View>;
+        });
+
+        return views;
+    }
+
+    _onInputButtonPressed(input) {
+        switch (typeof input) {
+            case 'number':
+                return this._handleNumberInput(input);
+            default:
+                return this._handleStringInput(input);
+        }
+    }
+
+    _handleNumberInput(num) {
+        let inputValue = (this.state.inputValue * 10) + num;
+
+        this.setState({
+            inputValue: inputValue
+        });
+    }
+
+    _handleStringInput(str) {
+        switch (str) {
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+                this.setState({
+                    selectedSymbol: str,
+                    previousInputValue: this.state.inputValue,
+                    inputValue: 0
+                });
+                break;
+
+            case '=':
+                let symbol = this.state.selectedSymbol,
+                    inputValue = this.state.inputValue,
+                    previousInputValue = this.state.previousInputValue;
+
+                if (!symbol) {
+                    return;
+                }
+
+                this.setState({
+                    previousInputValue: 0,
+                    inputValue: eval(previousInputValue + symbol + inputValue),
+                    selectedSymbol: null
+                });
+                break;
+
+            case 'ce':
+                this.setState(this.initialState);
+                    break;
+
+            case 'c':
+                this.setState({inputValue: 0});
+                break;
+
+        }
+    }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
